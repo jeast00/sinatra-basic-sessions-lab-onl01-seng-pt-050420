e86bce227dacc2e7b7205c6db6d6087c367defaa
Jonathan East <jonathan_east@ymail.com>
2020-07-31 17:45:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   
   post '/checkout' do
     
-    session['name'] = params[:item]
+    session['name'] = params['checkout']
     @item = session
     erb :checkout
   end
